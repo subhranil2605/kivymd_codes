3164540a8b3715773518eefb6b4668442927ad56
subhranil <subhranil2605@gmail.com>
2021-05-19 04:44:02 +0530

    project added

--- note_keeper_app/main.py
+++ note_keeper_app/main.py
@@ -1,9 +1,44 @@
-from utils.database_helper import DatabaseHelper
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
 
-database = DatabaseHelper()
+KV = '''
+MDScreen:
+    
+    MDBoxLayout:
+        orientation: 'vertical'
+        spacing: 20
+        padding: 30
+        adaptive_height: True
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        
+        MDLabel:
+            text: "Add Note"
+            halign: 'center'
+            font_size: 35
+        
+        MDTextField:
+            id: title
+            hint_text: "Title"
+            
+        MDTextField:
+            id: descrip
+            hint_text: "Description"
+        
+        MDRaisedButton:
+            text: "Add"
+            pos_hint: {"center_x": 0.5, "center_y": 0.5}
+            on_press: app.add_note()
+'''
 
 
-database.initialize_database()
-print("Success")
+class MainApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
 
-database.insertNote(0 ,'title', 'desci', '19.05.2021')
+    def add_note(self):
+        print(self.root.ids.title.text)
+        print(self.root.ids.descrip.text)
+
+
+if __name__ == '__main__':
+    MainApp().run()
--- note_keeper_app/utils/database_helper.py
+++ note_keeper_app/utils/database_helper.py
@@ -1,32 +1,14 @@
-import os
 import sqlite3
 
-try:
-    conn = sqlite3.connect('notes.db')
-    cur = conn.cursor()
 
-except Exception as e:
-    print(str(e))
+conn = sqlite3.connect('notes.db')
+cur = conn.cursor()
 
-class DatabaseHelper:
-    noteTable = 'note_table'
-    colId = 'id'
+cur.execute('''
+CREATE TABLE IF NOT EXISTS note_table (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)
+''')
+print('Data table created')
 
-    colTitle = 'title'
-    colDescription = 'description'
-    colDate = 'date'
+cur.execute('INSERT INTO note_table (title, description) VALUES ("my_title", "my_description")')
 
-    def _create_table(self, db):
-        cur = db.cursor()
-        cur.execute(f'''
-            CREATE TABLE {self.noteTable}({self.colId} INTEGER PRIMARY KEY AUTOINCREMENT, {self.colTitle} TEXT, {self.colDescription} TEXT, {self.colDate} TEXT)
-        ''')
-
-    def insertNote(self,id, title, des, date):
-        con = sqlite3.connect('notes.db')
-        cur = con.cursor()
-
-        cur.execute(f"INSERT INTO {self.noteTable} VALUES (?, ?, ?, ?)", (id, title, des, date))
-        # con.close()
-
-
+conn.close()

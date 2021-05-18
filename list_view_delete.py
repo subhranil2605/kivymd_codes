37746d7e1d9fb0108375ec91a0fd24f32633b1b8
subhranil <subhranil2605@gmail.com>
2021-05-18 17:17:02 +0530

    list view delete added

---
+++ list_view_delete.py
@@ -0,0 +1,46 @@
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+from kivymd.uix.list import OneLineListItem
+
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+class MainApp(MDApp):
+    counter = 0
+    note_list = []
+
+    def build(self):
+        if len(self.note_list) == 0:
+            self.note_list = []
+            self.update_list()
+
+        return Builder.load_file("list_view_delete.kv")
+
+    def on_start(self):
+        lists = self.root.ids.list
+        for i in range(self.counter):
+            lists.add_widget(OneLineListItem(
+                text=str(self.note_list[i]),
+                on_press=self.delete_note
+            ))
+
+    def delete_note(self, obj):
+        val = obj.text
+        values.remove(int(val))
+        print(values)
+
+        self.update_list()
+
+    def update_list(self):
+        self.note_list = values
+        self.counter = len(values)
+        self.update_list_view()
+
+    def update_list_view(self):
+        self.root.ids.list.clear_widgets()
+        self.on_start()
+
+
+
+if __name__ == '__main__':
+    MainApp().run()
